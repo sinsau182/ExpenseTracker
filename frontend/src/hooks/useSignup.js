eafc051bed14e6569e8f154d15fc417d0068f70c
sinsau182 <singhsaurav182001@gmail.com>
2024-09-16 03:31:17 +0530

    signup

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -5,12 +5,13 @@ export const useSignup = () => {
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(null)
   const { dispatch } = useAuthContext()
+  const BASE_URL = process.env.REACT_APP_BASE_URL
 
   const signup = async (username, email, password) => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('http://localhost:3000/api/user/signup', {
+    const response = await fetch(`${BASE_URL}user/signup`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ username, email, password })
