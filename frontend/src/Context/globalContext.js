2f61fb104ef9ab74696566e4baad8a63c160dec0
sinsau182 <singhsaurav182001@gmail.com>
2024-09-16 02:18:44 +0530

    domain

--- frontend/src/Context/globalContext.js
+++ frontend/src/Context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react'
 import axios from 'axios'
 import { useAuthContext } from '../hooks/useAuthContext';
 
-const BASE_URL = "http://localhost:5000/api/v1/";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const GlobalContext = React.createContext()
 
